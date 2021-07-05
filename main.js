f9bc865d0e2d513b63fac336465669736f333c9f
ShirishSharan <72022954+ShirishSharan@users.noreply.github.com>
2021-07-05 18:46:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+status="";
+img="";
+
+
+function preload(){
+    img=loadImage('dog_cat.jpg');
+
+}
+
+
+
+function setup(){
+canvas = createCanvas(640,420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+document.getElementById("status").innerHTML = "Status: Detecting Objects";
+
+}
+
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status = "true";
+    objectDetector.detect(img, gotResult);
+
+
+}
+
+
+function gotResult(error,results){
+    if(error)
+    {
+    console.error(error);
+   }console.log(results);
+
+}
+
+
+
+function draw(){
+image(img,0 , 0 , 640 , 420 );
+fill("FF0000");
+Text("Dog",45,75);
+noFill();
+stroke("#FF0000");
+rect(30,60,450,350);
+
+fill("blue");
+Text("Cat",320,120);
+noFill()
+stroke("blue");
+rect(300,90,270,320);
+}
