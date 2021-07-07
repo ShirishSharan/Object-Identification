75775bd38704914912165e4c89510ebd13f8f923
ShirishSharan <72022954+ShirishSharan@users.noreply.github.com>
2021-07-07 17:45:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status="";
 img="";
+object=[];
 
 
 function preload(){
@@ -31,23 +32,32 @@ function gotResult(error,results){
     if(error)
     {
     console.error(error);
-   }console.log(results);
+
+   }
+   else 
+   {
+       console.log(results);
+       objects = results;
+   }
+
 
 }
 
 
 
 function draw(){
-image(img,0 , 0 , 640 , 420 );
-fill("FF0000");
-text("Dog",45,75);
-noFill();
-stroke("#FF0000");
-rect(30,60,450,350);
 
-fill("blue");
-text("Cat",320,120);
-noFill()
-stroke("blue");
-rect(300,90,270,320);
+    if (status != "")
+    {
+        for(i=0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , objects[i].x, objects[i].y);
+            noFill();
+            stroke("red");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+        }
+    }
 }
